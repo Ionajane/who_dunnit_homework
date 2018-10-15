3cf7de4fdd4badc7b56960fd4426ced3f6002830
ionawright <ionawright@live.com>
2018-10-15 22:53:26 +0100

    9_questions_completed

--- who_dunnit.js
+++ who_dunnit.js
@@ -5,11 +5,11 @@
 //   room: 'Library',
 //   weapon: 'Rope'
 // };
-//
+
 // const declareMurderer = function() {
 //   return `The murderer is ${scenario.murderer}.`;
-// }
-//
+// };
+
 // const verdict = declareMurderer();
 // console.log(verdict);
 
@@ -19,24 +19,24 @@
 
 // EPISODE TWO
 
-const murderer = 'Professor Plum';
+// const murderer = 'Professor Plum';
 
-const changeMurderer = function() {
-  murderer = 'Mrs. Peacock';
-}
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// };
 
-const declareMurderer = function() {
-  return `The murderer is ${murderer}.`;
-}
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// };
 
-changeMurderer();
-const verdict = declareMurderer();
-console.log(verdict);
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
 
 // OUTPUT:
-// There is two people assigned as the murderer but both are a constant which cannot be changed. The murderer is Mrs.Peacock
-//
-//
+// There is two people assigned as the murderer but both are a constant which cannot be changed. The murderer is Professor Plum
+
+
 // EPISODE THREE
 //
 // let murderer = 'Professor Plum';
@@ -44,95 +44,92 @@ console.log(verdict);
 // const declareMurderer = function() {
 //   let murderer = 'Mrs. Peacock';
 //   return `The murderer is ${murderer}.`;
-// }
-//
+// };
+
 // const firstVerdict = declareMurderer();
 // console.log('First Verdict: ', firstVerdict);
-//
+
 // const secondVerdict = `The murderer is ${murderer}.`;
 // console.log('Second Verdict: ', secondVerdict);
-//
+
 // // OUTPUT:
 // The murderer is Mrs Peacock.
 // First Verdict: Mrs Peacock.
 // The murderer is Professor Plum.
 // Second Verdict: Professor Plum.
-//
-//
+
+
 // // EPISODE FOUR
-//
+
 // let suspectOne = 'Miss Scarlet';
 // let suspectTwo = 'Professor Plum';
 // let suspectThree = 'Mrs. Peacock';
-//
+
 // const declareAllSuspects = function() {
 //   let suspectThree = 'Colonel Mustard';
 //   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
-// }
-//
+// };
+
 // const suspects = declareAllSuspects();
 // console.log(suspects);
 // console.log(`Suspect three is ${suspectThree}.`);
-//
+
 // // OUTPUT:
 // The suspects are Miss Scarlet, Professor Plum, Colonel Mustard.
 // Suspect three is Mrs.Peacock
-//
-//
+
+
 // // EPISODE FIVE
-//
+
 // const scenario = {
 //   murderer: 'Miss Scarlet',
 //   room: 'Kitchen',
 //   weapon: 'Candle Stick'
 // };
-//
+
 // const changeWeapon = function(newWeapon) {
 //   scenario.weapon = newWeapon;
-// }
-//
+// };
+
 // const declareWeapon = function() {
 //   return `The weapon is the ${scenario.weapon}.`;
-// }
-//
+// };
+
 // changeWeapon('Revolver');
 // const verdict = declareWeapon();
 // console.log(verdict);
-//
-// // OUTCOME:
-// The weapon is the Candle Stick.
-// Revolver
-//
-//
+
+// OUTCOME:
+// The weapon is the Revolver
+
+
 // // EPISODE SIX
-//
+
 // let murderer = 'Colonel Mustard';
 //
 // const changeMurderer = function() {
 //   murderer = 'Mr. Green';
-//
+
 //   const plotTwist = function() {
 //     murderer = 'Mrs. White';
-//   }
-//
+//   };
 //   plotTwist();
-// }
-//
+// };
+
 // const declareMurderer = function () {
 //   return `The murderer is ${murderer}.`;
-// }
-//
+// };
+
 // changeMurderer();
 // const verdict = declareMurderer();
 // console.log(verdict);
-//
-// // OUTCOME
-// The murderer is Colonel Mustard.
-// Mr Green
-//
-//
-// // EPISODE SEVEN
-//
+
+// OUTCOME -----------------------?
+// The murderer is Mrs.White
+
+
+// EPISODE SEVEN
+
 // let murderer = 'Professor Plum';
 //
 // const changeMurderer = function() {
@@ -158,15 +155,13 @@ console.log(verdict);
 // changeMurderer();
 // const verdict = declareMurderer();
 // console.log(verdict);
-//
-// // OUTCOME:
-// The murderer is Professor Plum.
-// Mr Green.
-//
-//
-//
-// // EPISODE EIGHT
-//
+
+// OUTCOME:
+// The murderer is Mr Green.
+
+
+// EPISODE EIGHT
+
 // const scenario = {
 //   murderer: 'Mrs. Peacock',
 //   room: 'Conservatory',
@@ -201,14 +196,12 @@ console.log(verdict);
 // changeScenario();
 // const verdict = declareWeapon();
 // console.log(verdict);
-//
-// // OUTCOME:
-// The weapon is Lead Pipe.
-//
-//
-//
-// // EPISODE NINE
-//
+
+// OUTCOME:
+// The weapon is Candle Stick.
+
+// EPISODE NINE
+
 // let murderer = 'Professor Plum';
 //
 // if (murderer === 'Professor Plum') {
@@ -221,10 +214,9 @@ console.log(verdict);
 //
 // const verdict = declareMurderer();
 // console.log(verdict);
-//
-// // OUTCOME:
-// The murderer is Mrs.Peacock.
-//
+
+// OUTCOME:
+// The murderer is Professor Plum.
 
 
 /////////////////////////////////
